f843ef5de06a974ccf32e5e10ab7e7a99651815e
Diego Vasquez <botellongo69@outlook.com>
2020-02-19 18:22:17 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,28 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader :id
   
+  @@all = []
+  
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  def self.create_table
+    
+  end
+  
+  def self.drop_table
+    
+  end
+  
+  def save
+    
+  end
+  
+  def self.create
+    
+  end
 end
